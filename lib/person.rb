d4c326d6339437447558640b9e48dee864d70fb9
Kevin <kevinlai76@gmail.com>
2019-06-04 22:06:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,7 +6,17 @@ class Person
   def name
     @name
   end
+  
+  def job=(persons_job)
+    @job = persons_job
+  end
+  
+  def job
+    @job
+  end
 end
 
 Beyonce = Person.new
 Beyonce.name = "Beyonce"
+beyonce = Person.new
+beyonce.job = "Singer"
